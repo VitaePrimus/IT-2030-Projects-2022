de2b46905c5a7432cdf9669bd68eb3f94806f047
Vitaliy Vuychych <vitaliy202@gmail.com>
2022-04-09 19:48:54 -0400

    Commit

---
+++ Ch11Ex1TempManager/TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+using System;
+using System.Linq;
+
+
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller{
+        private TempManagerContext data { get; set; }
+        public ValidationController(TempManagerContext ctx){
+            data = ctx;
+        }
+        public JsonResult CheckDate(string date){
+            DateTime dt = DateTime.Parse(date);
+            Temp temp = data.Temps.FirstOrDefault(t => t.Date == dt);
+
+            if (temp == null)
+                return Json(true);
+            else
+                return Json($"Error! The date '{date}' is already in the database.");
+        }
+    }
+}
