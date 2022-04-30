046cae0a5bca8b8afa6969f6b434379bf29af230
Vitaliy Vuychych <vitaliy202@gmail.com>
2022-04-30 17:11:13 -0400

    Commit

---
+++ Project_2/FutureValue/Models/PriceQuotation.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Project_2.Models
+
+{
+    public class PriceQuotation
+    {
+        [Required(ErrorMessage = "Please enter subtotal price.")]
+        [Range(1, 9999999, ErrorMessage = "Subtotal price amount must be between 1 and 9999999.")]
+        public double Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter a discount percent")]
+        [Range(0, 100, ErrorMessage = "Discount percent must be between 0 and 100")]
+        public double Discount { get; set; }
+
+        public double CalculateDiscount()
+        {
+            double discount = Subtotal * (Discount * 0.01);
+
+            return discount;
+        }
+
+        public double CalculateTotal()
+        {
+            double discount = Subtotal * (Discount * 0.01);
+            double total = Subtotal - discount;
+
+            return total;
+        }
+
+    }
+}
