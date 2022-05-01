b1caa64a6400b2306f878406a67565e3c4c26341
Vitaliy Vuychych <vitaliy202@gmail.com>
2022-04-30 21:42:12 -0400

    commit

---
+++ Ch14Ex1ClassSchedule/ClassSchedule/Models/DataLayer/IClassScheduleUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassSchedule.Models
+{
+    public interface IClassScheduleUnitOfWork
+    {
+        //public Repository<Day> Days { get; }
+        //public Repository<Teacher> Teachers { get; }
+        //public Repository<Class> Classes { get; }
+
+        public IRepository<Day> Days { get; }
+        public IRepository<Teacher> Teachers { get; }
+        public IRepository<Class> Classes { get; }
+
+        public void Save();
+    }
+}
