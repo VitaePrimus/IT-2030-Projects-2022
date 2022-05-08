cde55eb0e0fd4aedadaadaaebcaf8008ac35b3e7
Vitaliy Vuychych <vitaliy202@gmail.com>
2022-05-07 22:23:13 -0400

    Commit

---
+++ Ch15Ex1ClassSchedule/ClassSchedule/Components/DayFilter.cs
@@ -0,0 +1,21 @@
+﻿using ClassSchedule.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassSchedule.Components
+{
+    public class DayFilter : ViewComponent{
+        private IRepository<Day> data { get; set; }
+        public DayFilter(IRepository<Day> rep) => data = rep;
+        public IViewComponentResult Invoke(){
+            var days = data.List(new QueryOptions<Day>{
+                OrderBy = d => d.DayId
+            });
+            
+            return View(days);
+        }
+    }
+}
---
+++ Ch15Ex1ClassSchedule/ClassSchedule/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ClassSchedule.Models;
+
+namespace ClassSchedule.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository<Class> data { get; set; }
+        public HomeController(IRepository<Class> rep) => data = rep;
+
+        public ViewResult Index(int id)
+        {
+            var classOptions = new QueryOptions<Class> {
+                Includes = "Teacher, Day"
+            };
+            if (id == 0) {
+                classOptions.OrderBy = c => c.DayId;
+            }
+            else {
+                classOptions.Where = c => c.DayId == id;
+                classOptions.OrderBy = c => c.MilitaryTime;
+            }
+
+            return View(data.List(classOptions));
+        }
+    }
+}
